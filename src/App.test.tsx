6fe294a2829232aea11c04f308ca0a1fb7e38641
Kwakcena <rhkrgudwh@gmail.com>
2022-04-17 17:53:24 +0900

    typescript 환경 설정을 하라
    typescript를 사용할 수 있는 환경을 구성하였습니다.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,4 @@
-import App from './App';
+import App from './App.jsx';
 
 import { render } from '@testing-library/react';
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
+/* eslint-disable */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
+  entry: `${path.resolve(__dirname, "./src")}/index.tsx`,
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.(ts|tsx|js|jsx)$/,
         exclude: /node_modules/,
         use: 'babel-loader',
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: `${path.resolve(__dirname, "./public")}/index.html`,
+    }),
+  ],
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".ts", ".jsx", ".tsx", ".css", ".json"],
   },
 };
