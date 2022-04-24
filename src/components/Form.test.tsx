63abcfe4b0c5873472915f314079eb5484f00e42
Kwakcena <rhkrgudwh@gmail.com>
2022-04-24 15:19:44 +0900

    아이디, 비밀번호, 게시글 번호를 입력할 창을 만들어라
    아이디, 비밀번호, 게시글 번호를 입력하는 창을 만들었습니다.

---
+++ src/components/Form.test.tsx
@@ -0,0 +1,17 @@
+import { render } from "@testing-library/react";
+
+import Form from './Form';
+
+describe('Form', () => {
+  const renderForm = () => render(
+    <Form />,
+  )
+
+  it('아이디, 비밀번호, 글 번호 입력창을 볼 수 있다', () => {
+    const { getByLabelText } = renderForm();
+
+    expect(getByLabelText('아이디를 입력해주세요')).toBeInTheDocument();
+    expect(getByLabelText('비밀번호를 입력해주세요')).toBeInTheDocument();
+    expect(getByLabelText('글 번호를 입력해주세요')).toBeInTheDocument();
+  });
+});
---
+++ src/components/Form.tsx
@@ -0,0 +1,38 @@
+import { Paper, Box, TextField, Typography } from "@mui/material"
+
+const style = {
+  paper: {
+    my: 3,
+  },
+  title: {
+    pt: 2,
+    pl: 2,
+  },
+  box: {
+    p: 2,
+    display: 'flex',
+    '& :not(:last-of-type)': {
+      mr: 3,
+    },
+  },
+} as const;
+
+export default function Form() {
+  return (
+    <Paper sx={style.paper} elevation={3}>
+      <Typography sx={style.title} variant="h5" component="h2">
+        아이디/비밀번호 입력
+      </Typography>
+      <Box
+        sx={style.box}
+        component="form"
+        noValidate
+        autoComplete="off"
+      >
+        <TextField label="아이디를 입력해주세요" variant="standard" />
+        <TextField label="비밀번호를 입력해주세요" type="password" variant="standard" />
+        <TextField label="글 번호를 입력해주세요" variant="standard" />
+      </Box>
+    </Paper>
+  )
+}
