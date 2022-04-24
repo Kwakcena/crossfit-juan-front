960714e08b026f2cd3c0866f4672f43593ce0a8b
Kwakcena <rhkrgudwh@gmail.com>
2022-04-24 21:55:16 +0900

    form에 제출 버튼을 구현하라
    form 을 작성하고 버튼을 누르면 제출이 되도록 구현합니다.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,6 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
-import { Paper, Box, TextField, Typography } from "@mui/material"
+import { Paper, Box, TextField, Typography, Button } from "@mui/material"
 
 const style = {
   paper: {
@@ -12,7 +12,9 @@ const style = {
   },
   box: {
     p: 2,
-    '& :not(:last-of-type)': {
+    display: 'flex',
+    alignItems: 'flex-end',
+    '& > :not(style)': {
       mr: 3,
     },
   },
@@ -29,12 +31,19 @@ export default function Form() {
     setForm({ ...form, [target.name]: target.value });
   }
 
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+
+    // TODO: 에약자 정보를 가져오는 api를 요청해야 합니다.
+  }
+
   return (
     <Paper sx={style.paper} elevation={3}>
       <Typography sx={style.title} variant="h5" component="h2">
         아이디/비밀번호 입력
       </Typography>
       <Box
+        onSubmit={handleSubmit}
         sx={style.box}
         component="form"
         noValidate
@@ -43,6 +52,7 @@ export default function Form() {
         <TextField name="id" onChange={handleChange} label="아이디를 입력해주세요" variant="standard" />
         <TextField name='password' onChange={handleChange} label="비밀번호를 입력해주세요" type="password" variant="standard" />
         <TextField name="articleNumber" onChange={handleChange} label="글 번호를 입력해주세요" variant="standard" />
+        <Button type="submit" variant="contained">제출하기</Button>
       </Box>
     </Paper>
   )
