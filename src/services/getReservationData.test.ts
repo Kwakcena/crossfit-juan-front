7beedc8894c90baed15dcec2e86b06833f457af1
Kwakcena <rhkrgudwh@gmail.com>
2022-05-05 20:09:29 +0900

    가장 많은 인원 수를 구한다
    각 수업에서 가장 많은 인원수를 구합니다.

---
+++ src/services/getReservationData.test.ts
@@ -0,0 +1,25 @@
+import { mockUserList } from "../../fixtures";
+
+import { fetchUserTimeTable } from "../api";
+import { getReservationData } from './getReservationData';
+
+jest.mock('../api');
+
+describe('getReservationData service', () => {
+  beforeEach(() => {
+    (fetchUserTimeTable as jest.Mock).mockResolvedValue(mockUserList.data);
+  })
+
+  it('예약 현황을 불러와서 각 수업의 인원, 가장 많은 인원의 수를 구한다', async () => {
+    const form = {
+      naverId: 'test123',
+      naverPw: 'asdqwe1234',
+      articleNumber: '1234',
+    }
+
+    const result = await getReservationData(form);
+
+    expect(result.maxPersons).toBe(13);
+    expect(result.timeTable).toEqual(mockUserList.data.timeTable);
+  });
+});
