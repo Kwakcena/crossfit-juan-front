e97928f71c50ad842ea6bc915f664d7692da7fd4
Kwakcena <rhkrgudwh@gmail.com>
2022-05-05 17:13:21 +0900

    article number를 선택하는 select box 컴포넌트 구현
    크롤링 할 수업 예약 글 목록을 선택할 수 있는 selectbox를 만들었습니다.

---
+++ src/components/SelectBox/SelectBox.test.tsx
@@ -0,0 +1,69 @@
+import { fireEvent, render, waitFor, within } from "@testing-library/react";
+
+import SelectBox from './SelectBox';
+
+describe('SelectBox component', () => {
+  given('articles', () => []);
+  given('selected', () => '');
+
+  const handleChange = jest.fn();
+
+  const renderSelectBox = () => render(
+    <SelectBox
+      articles={given.articles}
+      selected={given.selected}
+      onChange={handleChange}
+    />
+  )
+
+  it('label을 볼 수 있다', () => {
+    const { container } = renderSelectBox();
+
+    expect(container).toHaveTextContent('수업을 선택해주세요');
+  });
+
+  context('articles 가 있으면', () => {
+    given('articles', () => [
+      { title: '220504수업예약', articleNumber: '12345' },
+      { title: '220503수업예약', articleNumber: '12232' },
+      { title: '220502수업예약', articleNumber: '33232' },
+    ])
+
+    it('select box를 클릭헀을 때 article 목록을 볼 수 있다', async () => {
+      const { getAllByRole, getByRole } = renderSelectBox();
+
+      fireEvent.mouseDown(getByRole('button'));
+
+      given.articles.forEach((article: { title: string, articleNumber: string }, index: number) => {
+        expect(getAllByRole('option')[index]).toHaveTextContent(article.title);
+      })
+    });
+
+    it('article을 선택할 수 있다.', () => {
+      const { getByRole } = renderSelectBox();
+
+      fireEvent.mouseDown(getByRole('button'));
+
+      const listbox = within(getByRole('listbox'));
+
+      fireEvent.click(listbox.getByText('220502수업예약'));
+
+      expect(handleChange).toBeCalled();
+    });
+  });
+
+  context('선택된 값이 있으면', () => {
+    given('articles', () => [
+      { title: '220504수업예약', articleNumber: '12345' },
+      { title: '220503수업예약', articleNumber: '12232' },
+      { title: '220502수업예약', articleNumber: '33232' },
+    ])
+    given('selected', () => given.articles[0].articleNumber);
+
+    it('select box에 기본값으로 볼 수 있다', () => {
+      const { container } = renderSelectBox();
+
+      expect(container).toHaveTextContent(given.articles[0].title);
+    });
+  });
+});
---
+++ src/components/SelectBox/SelectBox.tsx
@@ -0,0 +1,39 @@
+import { Box, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import { ChangeEvent } from "react";
+import { ClassArticle } from "../../interfaces";
+
+interface Props {
+  articles: ClassArticle[]
+  onChange: (value: string) => void;
+  selected: string;
+}
+
+export default function SelectBox({
+  articles,
+  onChange,
+  selected,
+}: Props) {
+  const handleChange = ({ target: { value } }: SelectChangeEvent<string>) => {
+    onChange(value);
+  }
+
+  return (
+    <Box sx={{ minWidth: 200 }}>
+      <FormControl fullWidth size="small" variant="standard">
+        <InputLabel id="class-select-label">수업을 선택해주세요</InputLabel>
+        <Select
+          labelId="class-select-label"
+          id="select"
+          value={selected}
+          onChange={handleChange}
+        >
+          {articles.map(({ articleNumber, title }) => (
+            <MenuItem key={articleNumber} value={articleNumber}>
+              {title}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
+  )
+}
