72c96d3f48e6181d6394ed0470b07c0e1290dce9
Kwakcena <rhkrgudwh@gmail.com>
2022-05-05 19:01:31 +0900

    sessionStorage util 추가
    sessionStorage를 이용할 수 있는 util을 추가합니다.

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,6 +3,8 @@ import '@testing-library/jest-dom';
 import 'given2';
 import 'given2/setup';
 
+import 'jest-localstorage-mock';
+
 declare global {
   const given: {
     /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
