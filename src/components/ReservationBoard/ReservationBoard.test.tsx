2adcc1892068ec71c1c5a9a1e53d4d71abdad830
Kwakcena <rhkrgudwh@gmail.com>
2022-06-12 20:28:52 +0900

    Pc Timetable component refactoring
    timetable 컴포넌트의 이름을 수정하고 코드를 refactoring 합니다.

--- src/components/ReservationBoard/ReservationBoard.test.tsx
+++ src/components/ReservationBoard/ReservationBoard.test.tsx
@@ -1,18 +1,15 @@
 import { render } from "@testing-library/react";
 
 import { mockTimeTable, mockUserList } from "../../../fixtures";
+import { TIMES } from "../../constants/times";
 
-import TimeTable from './TimeTable';
+import ReservationBoard from './ReservationBoard';
 
 describe('TimeTable', () => {
   given('timeTable', () => ({}));
-  given('maxPersons', () => 0);
 
   const renderTimeTable = () => render(
-    <TimeTable
-      timeTable={given.timeTable}
-      maxPersons={given.maxPersons}
-    />,
+    <ReservationBoard timeTable={given.timeTable} />,
   )
 
   it('예약자 현황 title을 볼 수 있다.', () => {
@@ -32,36 +29,19 @@ describe('TimeTable', () => {
   });
 
   context('수업 시간, 예약자 데이터가 있으면', () => {
-    const times = ['0630', '0900', '1200', '1700', '1800', '1900', '2000', '2100', '2200'];
-
     given('timeTable', () => mockUserList.data.timeTable);
 
     it('수업 시간 목록과 예약자 현황을 볼 수 있다', () => {
       const { container } = renderTimeTable();
 
-      times.forEach((time) => {
+      TIMES.forEach((time) => {
         expect(container).toHaveTextContent(time);
       })
 
-      mockTimeTable[0].forEach(({ name, phone }) => {
+      mockTimeTable[0].forEach(({ name, phone }, index) => {
+        expect(container).toHaveTextContent(`${index + 1}`);
         expect(container).toHaveTextContent(name);
         expect(container).toHaveTextContent(phone);
-      })
-    });
-  });
-
-  context('최대 인원 수가 있으면', () => {
-    given('maxPersons', () => 13);
-
-    it('cell에 1부터 maxPersons 까지의 숫자를 볼 수 있다', () => {
-      const { getByRole } = renderTimeTable();
-
-      const numbers = Array.from({ length: given.maxPersons }, (_, index) => index + 1);
-
-      numbers.forEach((value) => {
-        const cell = getByRole('columnheader', { name: value.toLocaleString() });
-
-        expect(cell).toBeInTheDocument();
       })
     });
   });
