05a3b4bf5292dc7eebb795153c1cb9112e81f70a
Kwakcena <rhkrgudwh@gmail.com>
2022-06-12 20:34:10 +0900

    실제 데이터로 수정
    실제 데이터로 수정합니다.

--- src/services/getReservationData.ts
+++ src/services/getReservationData.ts
@@ -1,11 +1,9 @@
-import { mockUserList } from "../../fixtures";
 import { fetchUserTimeTable } from "../api"
 
 import { Form } from "../interfaces"
 
 export const getReservationData = async (form: Form) => {
-  // const { timeTable } = await fetchUserTimeTable(form);
-  const { timeTable } = mockUserList.data;
+  const { timeTable } = await fetchUserTimeTable(form);
 
   return {
     timeTable,
