ff8ac72b8b90c6baff3b736de1f8d7f6a615bab4
Kwakcena <rhkrgudwh@gmail.com>
2022-12-27 01:28:25 +0900

    react-query의 기본 설정 추가
    react-query의 기존 설정들을 추가합니다.

--- src/index.tsx
+++ src/index.tsx
@@ -7,13 +7,28 @@ import App from "./App";
 import { store } from "./store";
 
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const root = createRoot(document.getElementById("app") as Element);
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
 
 serviceWorkerRegistration.register();
