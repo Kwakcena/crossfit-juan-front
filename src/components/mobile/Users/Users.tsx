11121a0e60a050e98a52c23eda6a169a858ea5e6
hyeongjoGwak <hyeongjo.Gwak@kurlycorp.com>
2023-01-02 23:25:51 +0900

    중복되는 Key가 없도록 수정하라
    element의 key가 중복되지않도록 수정합니다.

--- src/components/mobile/Users/Users.tsx
+++ src/components/mobile/Users/Users.tsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 import { isEmpty } from "lodash";
 
-import { User } from "../../../interfaces"
+import { User } from "../../../interfaces";
 
 const List = styled.ul`
   list-style: none;
@@ -10,7 +10,7 @@ const List = styled.ul`
   display: flex;
   flex-wrap: wrap;
   background-color: #f2f2f2;
-`
+`;
 
 const Item = styled.li`
   position: relative;
@@ -21,44 +21,39 @@ const Item = styled.li`
   @media (min-width: 375px) {
     width: 20%;
   }
-`
+`;
 
 const UserCount = styled.span`
   position: absolute;
   font-size: 0.5rem;
   top: 2px;
   left: 4px;
-`
+`;
 
 const EmptyMessage = styled.div`
   text-align: center;
   padding: 8px 0;
   background-color: #f2f2f2;
-`
+`;
 
 interface Props {
   users: User[];
 }
 
-export default function Users({
-  users,
-}: Props) {
+export default function Users({ users }: Props) {
   if (isEmpty(users)) {
-    return (
-      <EmptyMessage>예약자가 없습니다.</EmptyMessage>
-    )
+    return <EmptyMessage>예약자가 없습니다.</EmptyMessage>;
   }
 
   return (
     <List>
       {users.map((user, index) => (
-        <Item key={user.phone}>
+        <Item key={`${user.name}-${user.phone}`}>
           <UserCount>{index + 1}</UserCount>
           {user.name}
-          <br />
-          ({user.phone})
+          <br />({user.phone})
         </Item>
       ))}
     </List>
-  )
+  );
 }
