a1d9232dc628d6744b812cc55399e348dfd28ac9
hyeongjoGwak <hyeongjo.Gwak@kurlycorp.com>
2023-01-18 00:42:50 +0900

    Loading 상태를 수정하라
    서버오류가 났을 때 로딩 상태가 제대로 업데이트 되지 않아서 스피너가 사라지지 않는 문제를 수정합니다.

--- src/queries/useQueriesLoading.tsx
+++
@@ -1,8 +0,0 @@
-import { useIsFetching, useIsMutating } from "react-query";
-
-export default function useQueriesLoading() {
-  const isFetching = useIsFetching();
-  const isMutating = useIsMutating();
-
-  return isFetching || isMutating ? true : false;
-}
