129da2f8f6d3cdc949d90c84fb54f1034de5265a
Kwakcena <rhkrgudwh@gmail.com>
2023-01-31 00:39:52 +0900

    실제 스토어 사용 테스트, 잘못 예약한 유저 컴포넌트
    - 실제 스토어를 사용하는 테스트 코드를 추가합니다.
    - 잘못 예약한 유저를 보여주는 컴포넌트를 구현합니다.

--- src/store.ts
+++ src/store.ts
@@ -1,23 +1,29 @@
 import {
   configureStore,
+  combineReducers,
   ThunkAction,
 } from "@reduxjs/toolkit";
+import type { PreloadedState } from "@reduxjs/toolkit";
 
-import { AnyAction, combineReducers } from "redux";
+import { AnyAction } from "redux";
 
 import appReducer from './slices/slice';
 import pageReducer from './slices/page';
 
-const reducer = combineReducers({
+const rootReducer = combineReducers({
   app: appReducer,
   page: pageReducer,
 })
 
-export const store = configureStore({
-  reducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ thunk: true }),
-})
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ thunk: true }),
+    preloadedState,
+  })
+}
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof rootReducer>
+export type AppStore = ReturnType<typeof setupStore>
+export type AppDispatch = AppStore['dispatch']
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, AnyAction>
