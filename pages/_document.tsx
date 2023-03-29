60d9bdee5200ab6d523e5155bad2a172909700e4
Kwakcena <rhkrgudwh@gmail.com>
2023-03-30 00:21:00 +0900

    nextjs 버전 12로 낮춤
    - nextjs 버전을 12로 낮춥니다.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,50 +3,22 @@ import { Head, Html, Main, NextScript } from "next/document";
 const Document = () => (
   <Html lang="ko">
     <Head>
-      <link rel="shortcut icon" href="favicon.ico" />
-      <link rel="icon" href="favicon.ico" />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/images/icons/favicon-16x16.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/images/icons/favicon-32x32.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="128x128"
-        href="/images/icons/icon-128x128.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="144x144"
-        href="/images/icons/icon-144x144.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/images/icons/icon-180x180.png"
-      />
-      <link
-        rel="apple-touch-icon"
-        sizes="192x192"
-        href="/images/icons/icon-192x192.png"
-      />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/images/icons/favicon-16x16.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/images/icons/favicon-32x32.png" />
+      <link rel="apple-touch-icon" sizes="128x128" href="/images/icons/icon-128x128.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="/images/icons/icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/images/icons/icon-180x180.png" />
+      <link rel="apple-touch-icon" sizes="192x192" href="/images/icons/icon-192x192.png" />
+
       <meta name="theme-color" content="#ffffff" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-      />
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
 
       <link rel="manifest" href="/manifest.json" />
 
-      <title>Crossfit juan reservation</title>
+      <title>크로스핏 주안</title>
     </Head>
     <body>
       <Main />
--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,12 +1,27 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
+const STATIC_FILE_PATTERN_LIST = [
+  /^(\/manifest\.json)/,
+  /^(\/images\/)/,
+  /^(\/naver)/,
+  /^(\/sitemap\/)/,
+  /^(\/service-worker\.js)/,
+];
+
 const MOBILE_USER_AGENT = new RegExp(
   '.*Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)|Tablet|tablet.*',
 );
 
 const checkRegex = (regex: RegExp) => (target: string): boolean => regex.test(target);
 const checkMobileUserAgent = checkRegex(MOBILE_USER_AGENT);
+const checkStaticFileRequest = (pathname: string): boolean =>
+  STATIC_FILE_PATTERN_LIST.reduce<boolean>((acc, regex) => {
+    if (regex.test(pathname)) {
+      acc = true;
+    }
+    return acc;
+  }, false);
 
 export function middleware(request: NextRequest) {
   const isMobile = checkMobileUserAgent(request.headers.get('user-agent') || '');
@@ -14,7 +29,9 @@ export function middleware(request: NextRequest) {
   const { nextUrl } = request;
   const { pathname } = nextUrl;
 
-  if (!isMobile) {
+  const isStaticFileRequest = checkStaticFileRequest(pathname);
+
+  if (isStaticFileRequest || !isMobile) {
     return NextResponse.next();
   }
   nextUrl.pathname = `/m${pathname}`;
@@ -22,5 +39,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: ['/((?!api|_next/static|_next/image).*)'],
 }
