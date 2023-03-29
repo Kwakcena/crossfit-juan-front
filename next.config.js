f342457500830a7ee33d467dd898168674270381
hyeongjoGwak <hyeongjo.gwak@kurlycorp.com>
2023-03-29 19:44:48 +0900

    Add PWA config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires, import/no-extraneous-dependencies */
+const withPlugins = require("next-compose-plugins");
+const withPWA = require("next-pwa");
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -5,6 +9,16 @@ const baseConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
+};
 
-module.exports = baseConfig;
+const pwaConfig = {
+  pwa: {
+    dest: "public",
+  },
+};
+
+const plugins = [[withPWA, pwaConfig]];
+
+const nextConfigWithPlugins = withPlugins(plugins, baseConfig);
+
+module.exports = nextConfigWithPlugins;
