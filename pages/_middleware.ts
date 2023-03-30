2960825e971aa5143a46bfaf38fb6ae373f4e094
hyeongjoGwak <hyeongjo.gwak@kurlycorp.com>
2023-03-30 13:16:54 +0900

    Check user-agent with console.log

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -31,7 +31,7 @@ export function middleware(request: NextRequest) {
     request.headers.get("user-agent") || ""
   );
 
-  console.log("request: ", request);
+  console.log("request: ", request.headers.get("user-agent"));
 
   const { nextUrl } = request;
   const { pathname } = nextUrl;
