276cf58b47a85b528a9a01a9ee595daaf41b3214
Kwakcena <rhkrgudwh@gmail.com>
2023-04-04 17:34:48 +0900

    CORS 문제 수정

--- src/services/getClassArticles.ts
+++ src/services/getClassArticles.ts
@@ -3,10 +3,9 @@ import { head } from "lodash";
 import axios from "axios";
 
 import { ClassArticle } from "../interfaces";
-import { API_URL } from "../../config";
 
 export const getClassArticles = async () => {
-  const { data } = await axios.get(`${API_URL}/api/articles`);
+  const { data } = await axios.get('/api/articles');
 
   return {
     articles: data.data,
