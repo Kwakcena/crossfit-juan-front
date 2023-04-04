cf3493fb7da10b101fad93b2a37a7e32c0cfb25d
Kwakcena <rhkrgudwh@gmail.com>
2023-04-04 15:24:24 +0900

    실제 URL 호출
    article 목록을 불러오는 api를 실제로 호출합니다.

--- pages/api/articles.ts
+++ pages/api/articles.ts
@@ -1,33 +1,59 @@
+import axios from "axios";
+import { get, map, slice } from "lodash";
+import { nanoid } from "nanoid";
+import queryString from "query-string";
 import type { NextApiRequest, NextApiResponse } from "next"
 
-const mockData = [
-  {
-    url: "https://m.cafe.naver.com/ArticleRead.nhn?clubid=28152386&articleid=16085&boardtype=L&menuid=99",
-    articleNumber: 16085,
-    title: "230404화 수업예약",
-  },
-  {
-    url: "https://m.cafe.naver.com/ArticleRead.nhn?clubid=28152386&articleid=16080&boardtype=L&menuid=99",
-    articleNumber: 16080,
-    title: "230403월 수업예약",
-  },
-  {
-    url: "https://m.cafe.naver.com/ArticleRead.nhn?clubid=28152386&articleid=16071&boardtype=L&menuid=99",
-    articleNumber: 16071,
-    title: "230331금 수업예약",
-  },
-  {
-    url: "https://m.cafe.naver.com/ArticleRead.nhn?clubid=28152386&articleid=16064&boardtype=L&menuid=99",
-    articleNumber: 16064,
-    title: "230330목 수업예약",
-  },
-  {
-    url: "https://m.cafe.naver.com/ArticleRead.nhn?clubid=28152386&articleid=16060&boardtype=L&menuid=99",
-    articleNumber: 16060,
-    title: "230329수 수업예약",
-  },
-]
+const CAFE_ID = 28152386;
+const articleListQuery = {
+  'search.clubid': CAFE_ID,
+  'search.queryType': 'lastArticle',
+  'search.menuid': 99,
+  'search.page': 1,
+  'search.perPage': 50,
+  ad: true,
+  uuid: nanoid(36),
+  adUnit: 'MW_CAFE_ARTICLE_LIST_RS',
+}
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ data: mockData })
+const createUrl = (url: URL, searchParams: URLSearchParams) => `${url.toString()}${searchParams ? `?${searchParams.toString()}` : ''}`;
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const articleListQueryString = queryString.stringify(articleListQuery);
+
+  const requestURL = createUrl(
+    new URL('https://apis.naver.com/cafe-web/cafe2/ArticleListV2.json'),
+    new URLSearchParams(articleListQueryString),
+  );
+
+  const { data } = await axios.get(requestURL);
+
+  const articleList = slice(get(data, 'message.result.articleList'), 0, 5);
+
+  const result = map(articleList, (article) => {
+    const articleId = get(article, 'item.articleId');
+    const subject = get(article, 'item.subject');
+    const articleReadQueryString = queryString.stringify({
+      clubid: CAFE_ID,
+      articleid: articleId,
+      boardtype: 'L',
+      menuid: '99',
+    })
+
+    const href = createUrl(
+      new URL('https://m.cafe.naver.com/ArticleRead.nhn'),
+      new URLSearchParams(articleReadQueryString),
+    );
+
+    return {
+      url: href,
+      articleNumber: articleId,
+      title: subject,
+    };
+  });
+
+  res.status(200).json({
+    success: true,
+    data: result,
+  });
 }
